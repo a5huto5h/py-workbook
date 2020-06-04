62186132fff0586cd61a710f9cbc819732f9a80a
Ashutosh Pandey <ashutosh.pandey@gmail.com>
2020-06-04 23:03:51 +0530

    Refactored and made PEP8 compliant

--- 004_exercise.py
+++ 004_exercise.py
@@ -1,10 +1,12 @@
 # Exercise 4: Area of a Field
-# Create a program that reads the length and width of a farmer’s field from the user in feet.
-# Display the area of the field in acres. Hint: There are 43,560 square feet in an acre.
+# Create a program that reads the length and width of a farmer’s field from
+# the user in feet. Display the area of the field in acres. Hint: There are
+# 43,560 square feet in an acre.
 
-def calculate_area(w, l):
-    area = w * l
+def calculate_area(width, length):
+    area = width * length
     return area
+
 
 def to_acres(sqft):
     sqft_to_acre = 1 / 43560
@@ -16,4 +18,3 @@ if __name__ == "__main__":
     [w, l] = dimensions.split()
     area_in_acres = to_acres(calculate_area(float(w), float(l)))
     print("Area of the field is %.4f acres" % area_in_acres)
-
