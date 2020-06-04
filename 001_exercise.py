39ac90b37639ab32dfc4e925e49112bf3c4a6130
Ashutosh Pandey <ashutosh.pandey@gmail.com>
2020-06-04 19:57:55 +0530

    changed address

--- 001_exercise.py
+++ 001_exercise.py
@@ -1,1 +1,1 @@
-def print_address():    print("Ashutosh Pandey")    print("SB-302, Shriram Spandhana Apartments,")    print("Challaghatta, Yemalur Post,")    print("Bengaluru 560037")    print("Karnataka, India")if __name__ == "__main__":    print_address()
+def print_address():    print("Ashutosh Pandey")    print("House No., Apartment Name,")    print("Area, Post Office,")    print("Bengaluru 560037")    print("Karnataka, India")if __name__ == "__main__":    print_address()
